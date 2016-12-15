5b23ab6ccefaf194be0f6b3ce8ad86d4e3b6dcc6
James Ahlm <14782828428@qq.com>
2016-12-15 20:37:20 +0800

    Added AddRecipe To Controller

--- app/controllers/dist/index.js
+++ app/controllers/dist/index.js
@@ -9,20 +9,111 @@ var Controller = React.createClass({
 　　getInitialState(){
 	return{recipes:　this.props.recipes, view: "menu", viewedRecipe: {}}
   },
+  addRecipe(r){
+    console.log(r);
+  },
   changeView(view, recipe){
     if(view == "view"){this.setState({viewedRecipe:recipe, view:"view"})}
     if(view == "menu"){this.setState({viewedRecipe:{}, view:"menu"})}
+　　　　if(view == "add"){this.setState({viewedRecipe:{},view:"add"})}
     console.log(view);
   },
   render(){
     var current;
 
     if(this.state.view == "menu"){ current = <Menu recipes={this.state.recipes} changeView={this.changeView} />}
-
+    if(this.state.view == "add"){current = <AddRecipe addRecipe={this.addRecipe}/>}
     if(this.state.view == "view"){current = <Viewer recipe={this.state.viewedRecipe} changeView={this.changeView} />}
     return(<div className="row"><div className="col-md-6 col-md-offset-3"><div className="well well-lg">{current}</div></div></div>)
   }
 })
+
+
+var AddRecipe = React.createClass({
+　　getInitialState(){
+	return {recipeName: "", ingredients: [{id:0, name:""}]}
+  },
+  handleNameChange(e){
+    this.setState({recipeName:  e.target.value})
+  },
+  addRecipe(){
+    var recipe = {}
+    recipe.name = this.state.recipeName;
+    recipe.ingredients = this.state.ingredients;
+    this.props.addRecipe(recipe);
+  },
+  addIngredient(){
+    var ingredients = this.state.ingredients.slice();
+    var n = ingredients.length;
+    ingredients.push({id:n, name:""})
+    this.setState({ingredients: ingredients})
+  },
+  render() {
+
+    var that = this;
+
+    var ingredients = this.state.ingredients.map(function(item, i){
+      
+       var handleChange = function(e){
+	console.log(that.state.ingredients[i]);
+　　　　　　　　　var ingredients = that.state.ingredients;
+         ingredients[i].name = e.target.value;
+         that.setState({ingredients: ingredients});
+       };
+      
+       return <Ingredient key={item.id} id={item.id} value={item.name} handleChange={handleChange} />
+     })
+
+
+    return(
+          <form className="form-horizontal">
+	      
+　　　　　　　　　　　　　　<div className="panel panel-primary">
+                  <div className="panel-heading">
+                    <h3 className="panel-title text-center">Add Recipe</h3>
+                  </div>
+                  <div className="panel-body">
+		    <div className="form-group">
+		      <label className="col-md-3">Name</label>
+	              <div className="col-md-9">
+	                <input id="recipe-name" value={this.state.recipeName} onChange={this.handleNameChange} className="form-control" placeholder="Name" type="text"/>
+	              </div>
+	            </div>
+                    {ingredients}
+		    <div className="form-group">
+		      <div className="col-md-2 col-md-offset-4">
+	                <div className="btn btn-primary" onClick={this.addRecipe}>Add</div>
+		      </div>
+		      <div className="col-md-2">
+                        <div className="btn btn-default" onClick={this.addIngredient}>Add Ingredient</div>
+		     </div>
+	            </div>
+                  </div>
+               　</div>
+	      
+	    </form>
+    )
+  }
+})
+
+
+
+
+
+
+var Ingredient = React.createClass({
+  render() {
+    return (
+	<div className="form-group">
+	  <label　className="col-md-3">Ingredient {this.props.id + 1}</label>
+	  <div className="col-md-9">
+	    <input id="recipe-name" value={this.props.value} onChange={this.props.handleChange} className="form-control" placeholder="Ingredient" type="text" />
+	  </div>
+	</div>
+    );
+  }
+})
+
 
 
 
