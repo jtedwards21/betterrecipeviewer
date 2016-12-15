580fddc14dc2a2ab1f57513e32c1b64472fd316c
James Ahlm <14782828428@qq.com>
2016-12-15 08:36:14 +0800

    Can add boxes for multiple ingredients

--- app/controllers/dist/index.js
+++ app/controllers/dist/index.js
@@ -5,7 +5,7 @@
 
 var Box = React.createClass({
 　　getInitialState(){
-	return{recipeName: ""}
+	return{recipeName: "", ingredients: [{id:0, name:""}]}
   },
   handleNameChange(e){
     this.setState({recipeName:  e.target.value})
@@ -14,9 +14,28 @@ var Box = React.createClass({
 
   },
   addIngredient(){
-
+    var ingredients = this.state.ingredients.slice();
+    var n = ingredients.length;
+    ingredients.push({id:n, name:""})
+    this.setState({ingredients: ingredients})
   },
   render() {
+
+    var that = this;
+
+    var ingredients = this.state.ingredients.map(function(item, i){
+      
+       var handleChange = function(e){
+	console.log(that.state.ingredients[i]);
+　　　　　　　　　var ingredients = that.state.ingredients;
+         ingredients[i].name = e.target.value;
+         that.setState({ingredients: ingredients});
+       };
+      
+       return <Ingredient key={item.id} id={item.id} value={item.name} handleChange={handleChange} />
+     })
+
+
     return(
       <div className="row">
         <div className="col-md-6 col-md-offset-3">
@@ -31,6 +50,7 @@ var Box = React.createClass({
 		<div className="col-md-2 col-md-offset-4">
 	          <div className="btn btn-default" onClick={this.addRecipe}>Add</div>
 		</div>
+                {ingredients}
 		<div className="col-md-2">
                   <div className="btn btn-default" onClick={this.addIngredient}>Add Ingredient</div>
 		</div>
@@ -43,6 +63,22 @@ var Box = React.createClass({
 })
 
 
+
+
+
+
+var Ingredient = React.createClass({
+  render() {
+    return (
+	<div className="form-group">
+	  <label　className="col-md-2">Ingredient {this.props.id}</label>
+	  <div className="col-md-10">
+	    <input id="recipe-name" value={this.props.childValue} onChange={this.props.handleChange} className="form-control" placeholder="Ingredient" type="text" />
+	  </div>
+	</div>
+    );
+  }
+})
 
 
 ReactDOM.render(
