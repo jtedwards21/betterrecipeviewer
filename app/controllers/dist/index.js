8e44b179501cb7228f440906a23fef126a1cb8c3
James Ahlm <14782828428@qq.com>
2016-12-15 08:19:33 +0800

    Added a Box Input

--- app/controllers/dist/index.js
+++ app/controllers/dist/index.js
@@ -4,10 +4,40 @@
 
 
 var Box = React.createClass({
+　　getInitialState(){
+	return{recipeName: ""}
+  },
+  handleNameChange(e){
+    this.setState({recipeName:  e.target.value})
+  },
+  addRecipe(){
+
+  },
+  addIngredient(){
+
+  },
   render() {
     return(
-        <div className="square">
+      <div className="row">
+        <div className="col-md-6 col-md-offset-3">
+          <form className="form-horizontal">
+	      <div className="form-group">
+		<label className="col-md-2">Name</label>
+	        <div className="col-md-10">
+	          <input id="recipe-name" value={this.state.recipeName} onChange={this.handleNameChange} className="form-control" placeholder="Name" type="text"/>
+	        </div>
+	      </div>
+	      <div className="form-group">
+		<div className="col-md-2 col-md-offset-4">
+	          <div className="btn btn-default" onClick={this.addRecipe}>Add</div>
+		</div>
+		<div className="col-md-2">
+                  <div className="btn btn-default" onClick={this.addIngredient}>Add Ingredient</div>
+		</div>
+	      </div>
+	    </form>
 	</div>
+      </div>
     )
   }
 })
