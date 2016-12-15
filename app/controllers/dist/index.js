cb0f69e01b288d2783701525c94e57e57d4c0755
James Ahlm <14782828428@qq.com>
2016-12-15 08:10:15 +0800

    Initial commit

---
+++ app/controllers/dist/index.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+var Box = React.createClass({
+  render() {
+    return(
+        <div className="square">
+	</div>
+    )
+  }
+})
+
+
+
+
+ReactDOM.render(
+  <Box />,
+  document.getElementById('container')
+)
